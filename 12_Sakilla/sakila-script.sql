e7033cf110f15e9cee59c1b1b025edca5cd008ef
JoshuaHolloway <jhollow6@asu.edu>
2019-06-09 12:01:29 -0500

    part-6 sakila

--- 12_Sakilla/sakila-script.sql
+++ 12_Sakilla/sakila-script.sql
@@ -18,8 +18,26 @@
 -- WHERE actor.actor_id = 5
 
 -- ======================================================================================
--- PART-4
-SELECT * FROM customer
-JOIN address ON address.address_id = customer.address_id
-JOIN city ON city.city_id = address.city_id
-WHERE store_id = 1 and city.city_id = 1 or city.city_id = 42 or city.city_id = 312 or city.city_id = 459
+-- -- PART-4
+-- SELECT * FROM customer
+-- JOIN address ON address.address_id = customer.address_id
+-- JOIN city ON city.city_id = address.city_id
+-- WHERE store_id = 1 and city.city_id = 1 or city.city_id = 42 or city.city_id = 312 or city.city_id = 459
+
+-- ======================================================================================
+-- -- PART-5
+-- SELECT * FROM film_actor
+-- JOIN film ON film.film_id = film_actor.film_id
+-- WHERE film_actor.actor_id LIKE 15 AND film.rating LIKE 'G' AND film.special_features--  LIKE 'behind the scenes'
+
+-- ======================================================================================
+-- PART-6
+-- SELECT * FROM film
+SELECT * FROM film
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON film_actor.actor_id = actor.actor_id
+
+
+
+
+
