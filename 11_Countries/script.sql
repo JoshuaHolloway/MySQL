1ae447c6e5deabd5768a7af819abed09db8035c1
JoshuaHolloway <jhollow6@asu.edu>
2019-06-09 08:03:17 -0500

    only one question left on countries!

--- 11_Countries/script.sql
+++ 11_Countries/script.sql
@@ -4,11 +4,11 @@
 -- ORDER BY percentage DESC;
 -- =========================================================================================================
 -- -- Part 2
--- SELECT name, country_id FROM cities
--- SELECT count( DISTINCT(country_id) ) FROM cities
-
 -- https://stackoverflow.com/questions/1346345/mysql-count-the-occurrences-of-distinct-values
--- SELECT country_id,COUNT(*) as count FROM cities GROUP BY name ORDER BY count DESC;
+-- SELECT cities.country_id as Country_ID, countries.name AS Country_Name, COUNT(*) AS Number_of_Cities FROM cities
+-- JOIN countries ON countries.id = cities.country_id
+-- GROUP BY countries.name
+-- ORDER BY Number_of_Cities DESC;
 
 -- =========================================================================================================
 -- -- Part 3
@@ -33,4 +33,19 @@
 -- WHERE surface_area < 501 AND population > 100e3
 
 -- =========================================================================================================
--- Part 6
+-- -- Part 6
+-- SELECT name, government_form, capital, life_expectancy FROM countries
+-- WHERE capital > 200 AND government_form = 'Constitutional Monarchy' AND life_expectancy > 75
+-- order by life_expectancy DESC
+
+-- =========================================================================================================
+-- -- Part 7
+-- SELECT countries.name AS Country_Name, cities.name AS City_Name, district as District, cities.population as Country_Population FROM cities
+-- JOIN countries ON countries.id = cities.country_id
+-- WHERE cities.population > 500e3 AND cities.district = 'Buenos Aires' AND countries.name = 'Argentina'
+
+-- =========================================================================================================
+-- -- Part 8
+-- SELECT name AS Country_Name, region as Region_Name, COUNT(*) AS Number_of_Countries_in_Region FROM countries
+-- GROUP BY region
+-- ORDER BY Number_of_Countries_in_Region DESC;
