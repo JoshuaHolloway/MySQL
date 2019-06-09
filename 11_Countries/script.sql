c02f4c2a9656c03936c8060ce72c87f92a08d06d
JoshuaHolloway <jhollow6@asu.edu>
2019-06-08 21:56:14 -0500

    update

--- 11_Countries/script.sql
+++ 11_Countries/script.sql
@@ -1,7 +1,36 @@
+-- -- Part 1
+-- SELECT language, country_code, percentage FROM languages
+-- WHERE languages.language = "Slovene"
+-- ORDER BY percentage DESC;
+-- =========================================================================================================
+-- -- Part 2
+-- SELECT name, country_id FROM cities
+-- SELECT count( DISTINCT(country_id) ) FROM cities
 
+-- https://stackoverflow.com/questions/1346345/mysql-count-the-occurrences-of-distinct-values
+-- SELECT country_id,COUNT(*) as count FROM cities GROUP BY name ORDER BY count DESC;
 
-SELECT language, country_code, percentage FROM languages
-WHERE languages.language = "Slovene"
-ORDER BY percentage DESC;
+-- =========================================================================================================
+-- -- Part 3
+-- SELECT * FROM cities
+-- SELECT name, population, country_id FROM cities
+-- WHERE cities.population > 500e3 AND cities.country_id = 136
 
+--  SELECT * FROM countries
+--  WHERE countries.name = "Mexico"
 
+-- TODO: Attempt to join on country_id
+
+-- =========================================================================================================
+-- -- Part 4
+-- SELECT language, country_code, percentage FROM languages
+-- WHERE languages.percentage > 89
+-- ORDER BY percentage DESC;
+
+-- =========================================================================================================
+-- -- Part 5
+-- SELECT name, surface_area, population FROM countries
+-- WHERE surface_area < 501 AND population > 100e3
+
+-- =========================================================================================================
+-- Part 6
